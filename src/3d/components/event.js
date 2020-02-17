c3caec90e419a41a964fa8986aed763c665477f9
Birgitte Haavardsholm <birgitte.haavardsholm@gmail.com>
2020-02-17 20:01:13 +0100

    Move commom logic to event

--- src/3d/components/event.js
+++ src/3d/components/event.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import TextSprite from 'three.textsprite';
 import TextBox from './textbox';
 import TextBox2 from "./textbox0"
+import { show } from '../places/salardeuyuni/salardeuyuni';
 
 const createSprite = function(spriteMap, x, y, opacity){
   let spriteMaterial = new THREE.SpriteMaterial( { map: spriteMap, color: 0xffffff } );
@@ -30,7 +31,7 @@ const createPlane = function(texture, x, y, opacity){
 
 export default class Event{
 
-  constructor(text, color, position){
+  constructor(text, color, firstPosition, secondPosition){
     let sprite = new TextSprite({
       material: {
         color: color,
@@ -45,33 +46,49 @@ export default class Event{
     });
 
 
-    sprite.translateX(position.x);
-    sprite.translateY(position.y);
-    sprite.translateZ(position.z);
+    sprite.translateX(firstPosition.x);
+    sprite.translateY(firstPosition.y);
+    sprite.translateZ(firstPosition.z);
+
+    this.firstPositions = []
+    this.secondPositions = []
+    this.firstPositions.push(firstPosition);
+    this.secondPositions.push(secondPosition);
 
     this.event = new THREE.Object3D();
     this.event.add(sprite);
 
   }
 
-  addImage(image, position, size){
+  addImage(image, firstPosition, size, secondPosition){
     let sprite = createPlane(image, size.x,size.y);
 
-    sprite.translateX(position.x);
-    sprite.translateY(position.y);
-    sprite.translateZ(position.z);
+    sprite.translateX(firstPosition.x);
+    sprite.translateY(firstPosition.y);
+    sprite.translateZ(firstPosition.z);
+
+    this.firstPositions.push(firstPosition);
+    this.secondPositions.push(secondPosition);
 
     this.event.add(sprite);
   }
 
-  addTextBox(text, position){
-    let textBox = new TextBox(text, position)
-      this.event.add(textBox);
+  addTextBox(text, firstPosition,secondPosition){
+    let textBox = new TextBox(text, firstPosition)
+
+    this.firstPositions.push(firstPosition);
+    this.secondPositions.push(secondPosition);
+
+    this.event.add(textBox);
   }
 
-  addTextBox2(id, position){
-    let textBox = new TextBox2(id, position)
-      this.event.add(textBox);
+  addTextBox2(id, firstPosition,secondPosition){
+    let textBox = new TextBox2(id, firstPosition)
+
+    this.firstPositions.push(firstPosition);
+    this.secondPositions.push(secondPosition);
+
+    this.event.add(textBox);
   }
 
   addCross(color){
@@ -97,7 +114,52 @@ export default class Event{
     this.event.add(sprite);
   }
 
-  update(){
+  updatePos(object, finalDestination){
+    let delta = 0.05;//TODO use time
+    let pos = object.position.clone()
+    let dir = pos.sub(finalDestination);
+    object.position.sub(dir.multiplyScalar(delta));
+    if(dir.length() < 0.01){
+      return true;
+    }
+    return false;
+  }
+
+  updateScale(object, scale){
+    if( object.scale.x > scale){
+      object.scale.multiplyScalar(scale);
+    }
+  }
+
+  update(showPlace){
+    if(showPlace){
+      let isUpdated = false;
+      for(let i = 0; i < this.event.children.length-2; i++){
+        isUpdated = this.updatePos(this.event.children[i], this.secondPositions[i]);
+      }
+      this.updatePos(this.event.children[5].domObject,this.secondPositions[5]);
+
+
+      this.updateScale(this.event.children[4],0.666);
+      this.event.children[5].element.style.width = '50px';
+   
+      if(isUpdated){
+      this.event.children[6].element.style.color = 'black'; 
+      }
+    }else{
+      this.event.children[6].element.style.color = 'white';
+
+      for(let i = 0; i < this.event.children.length-2; i++){
+        this.updatePos(this.event.children[i], this.firstPositions[i]);
+      }
+      this.updatePos(this.event.children[5].domObject,this.firstPositions[5]);
+  
+     this.event.children[5].element.style.width = '20px';
+    }
+
 
   }
+
+
+
 };
--- src/3d/places/salardeuyuni/salardeuyuni.js
+++ src/3d/places/salardeuyuni/salardeuyuni.js
@@ -16,82 +16,22 @@ const init = function(inTimelinePos){
 
   timelinePos = inTimelinePos;
 
-  event = new Event("Salar de Uyuni", myBlue, new THREE.Vector3(2,0,4));
-  event.addImage( new THREE.TextureLoader().load( itest ), new THREE.Vector3(-3.5,1.5,-2), new THREE.Vector2(3,2));
-  event.addImage( new THREE.TextureLoader().load( itest2 ), new THREE.Vector3(1.5,3,0), new THREE.Vector2(3,2));
-  event.addImage( new THREE.TextureLoader().load( itest3 ), new THREE.Vector3(-2.5,-2,2), new THREE.Vector2(3,2));
-  event.addImage( new THREE.TextureLoader().load( itest4 ), new THREE.Vector3(1.5,-3.5,-4), new THREE.Vector2(2,3));
-  event.addTextBox(ingress,new THREE.Vector3(6,-4,timelinePos));
+  event = new Event("Salar de Uyuni", myBlue, new THREE.Vector3(2,0,4),new THREE.Vector3(-0.0,3.0,4));
+  event.addImage( new THREE.TextureLoader().load( itest ), new THREE.Vector3(-3.5,1.5,-2), new THREE.Vector2(3,2),new THREE.Vector3(-3.6,-0.0,4));
+  event.addImage( new THREE.TextureLoader().load( itest2 ), new THREE.Vector3(1.5,3,0), new THREE.Vector2(3,2),new THREE.Vector3(-0.6,0.0,4));
+  event.addImage( new THREE.TextureLoader().load( itest3 ), new THREE.Vector3(-2.5,-2,2), new THREE.Vector2(3,2),new THREE.Vector3(2.4,-0.0,4));
+  event.addImage( new THREE.TextureLoader().load( itest4 ), new THREE.Vector3(1.5,-3.5,-4), new THREE.Vector2(2,3),new THREE.Vector3(4.57,-0.0,4));
+  event.addTextBox(ingress,new THREE.Vector3(6,-4,timelinePos),new THREE.Vector3(0,0,4+timelinePos));
 
-
-   event.addTextBox2(new THREE.Vector3(0,-4,timelinePos+4.0));
+  event.addTextBox2(new THREE.Vector3(0,-4,timelinePos+4.0));
   //event.event.children[6].element.style.opacity = 0.00;
-
-
   event.event.translateZ(timelinePos);
 
   return event.event;
 }
 
 const update = function(){
-
-  if (showPlace){
-    
-    updatePos(event.event.children[0],new THREE.Vector3(-0.0,3.0,4));
-    updatePos(event.event.children[1],new THREE.Vector3(-3.6,-0.0,4));
-    updatePos(event.event.children[2],new THREE.Vector3(-0.6,0.0,4));
-    updatePos(event.event.children[3],new THREE.Vector3(2.4,-0.0,4));
-    updatePos(event.event.children[4],new THREE.Vector3(4.57,-0.0,4));
-    let isUpdated= updatePos(event.event.children[5].domObject,new THREE.Vector3(0,0,4+timelinePos));
-
-    updateScale(event.event.children[4],0.666);
-   event.event.children[5].element.style.width = '50px';
-
-    //TODO: add crossing out text
-    //if(!showX){
-     // event.addCross(myBlue);
-     // showX = true;
-    //}
-
-    if(isUpdated)
-    event.event.children[6].element.style.color = 'black'; 
-
-  }else{
-    event.event.children[6].element.style.color = 'white';
-
-    updatePos(event.event.children[0],new THREE.Vector3(2,0,4));
-    updatePos(event.event.children[1],new THREE.Vector3(-3.5,1.5,-2));
-    updatePos(event.event.children[2],new THREE.Vector3(1.5,3,0));
-    updatePos(event.event.children[3],new THREE.Vector3(-2.5,-2,2));
-    updatePos(event.event.children[4],new THREE.Vector3(1.5,-3.5,-4));
-    updatePos(event.event.children[5].domObject,new THREE.Vector3(6,-4,timelinePos));
-
-   event.event.children[5].element.style.width = '20px';
-
-  }
-}
-
-function updatePos(object, finalDestination){
-  let delta = 0.05;//TODO use time
-  //console.log(event.event.children)
-  //console.log("show place");
-  let pos = object.position.clone()
-  //console.log(pos, finalDestination);
-  let dir = pos.sub(finalDestination);
-//    console.log(dir.length());
-  object.position.sub(dir.multiplyScalar(delta));
-  if(dir.length() < 0.01){
-    return true;
-  }
-  return false;
-
-  
-}
-
-function updateScale(object, scale){
-  if( object.scale.x > scale){
-    object.scale.multiplyScalar(scale);
-  }
+  event.update(showPlace)
 }
 
 const show = function(){
